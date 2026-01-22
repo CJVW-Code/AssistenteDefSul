58aa81c6535d241867412119b261d7a6e0dc9828
Weslley <weslleysanchez123@gmail.com>
2026-01-22 16:44:42 -0300

    Correção documentos declarados

--- frontend/src/areas/cidadao/pages/FormularioSubmissao.jsx
+++ frontend/src/areas/cidadao/pages/FormularioSubmissao.jsx
@@ -1126,11 +1126,11 @@ export const FormularioSubmissao = () => {
         <div>
           <h3 className="font-bold text-special text-lg">Antes de começar</h3>
           <p className="text-muted mt-1">
-            Tenha em mãos seus documentos (RG, CPF, Comprovantes). 
+            Tenha em mãos seus documentos (RG, Comprovante de renda, Comprovante de Residência e Certidão de nascimento). 
             Para garantir a análise do seu caso, será necessário:
           </p>
           <ul className="list-disc list-inside text-muted text-sm mt-2 space-y-1 font-medium">
-            <li>Anexar pelo menos <strong>4 documentos</strong>;</li>
+            <li>Anexar pelo menos esses <strong> 4 documentos</strong>;</li>
             
           </ul>
         </div>
